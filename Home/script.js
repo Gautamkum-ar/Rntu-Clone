c569974dedb4523d7b5b3c2e7bc446f27dd696a3
GAUTAM KUMAR <gkvc9696@gmail.com>
2022-11-05 00:46:19 +0530

    first commit

---
+++ Home/script.js
@@ -0,0 +1,10 @@
+const admission = document.querySelector("#admission");
+const addSec = document.querySelector(".aadd-sec");
+addSec.style.display = "none";
+
+admission.addEventListener("click", () => {
+  addSec.style.display = "block";
+});
+addSec.style.display = "none";
+
+
